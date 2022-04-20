0097398ec597e9a848cec4f48abb659c7f76883b
Facundo Diaz <fdiaz@Facundos-MacBook-Pro.local>
2022-04-19 21:30:55 -0300

    cambios

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,12 +9,15 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
   // Root - Show all products
   index: (req, res) => {
-    // Do the magic
+    res.render("products", { products, toThousand });
   },
 
   // Detail - Detail from one product
   detail: (req, res) => {
-    // Do the magic
+    const productId = req.params.id;
+    const product = products.find((p) => p.id === Number(productId));
+
+    res.render("detail", { product });
   },
 
   // Create - Form to create
@@ -25,11 +28,16 @@ const controller = {
   // Create -  Method to store
   store: (req, res) => {
     let id = products[products.length - 1].id + 1;
+    let image = "default-image.png";
+    console.log(req.file);
+    if (req.file) {
+      image = req.file.filename;
+    }
 
     let newProduct = {
       id,
       ...req.body,
-      image: "default-image.png",
+      image,
     };
 
     products.push(newProduct);
@@ -40,16 +48,51 @@ const controller = {
 
   // Update - Form to edit
   edit: (req, res) => {
-    // Do the magic
+    const productId = req.params.id;
+    const productToEdit = products.find((p) => p.id === Number(productId));
+
+    res.render("product-edit-form", { productToEdit });
   },
   // Update - Method to update
   update: (req, res) => {
-    // Do the magic
+    const productId = Number(req.params.id);
+    let productToEdit = products.find((p) => p.id === productId);
+
+    let image = "default-image.png";
+    console.log(req.file);
+    if (req.file) {
+      image = req.file.filename;
+    }
+
+    productToEdit = {
+      ...req.body,
+      id: productId,
+      image,
+    };
+
+    const updatedProducts = products.map((p) => {
+      if (p.id === productToEdit.id) {
+        return (p = { ...productToEdit });
+      }
+      return p;
+    });
+
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(updatedProducts),
+      "utf-8"
+    );
+
+    res.redirect("/");
   },
 
   // Delete - Delete one product from DB
   destroy: (req, res) => {
-    // Do the magic
+    const productId = Number(req.params.id);
+    const finalProducts = products.filter((p) => p.id != productId);
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts), "utf-8");
+    res.redirect("/");
   },
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,25 +1,44 @@
 // ************ Require's ************
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const router = express.Router();
 
 // ************ Controller Require ************
 const productsController = require("../controllers/productsController");
 
+//MULTER CONFIG
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let folder = path.join(__dirname, "../../public/images/products");
+    console.log(folder);
+    cb(null, folder);
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    const fileName = Date.now() + "-" + file.originalname;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage });
+
 // /*** GET ALL PRODUCTS ***/
-// router.???('/', productsController.index);
+router.get("/", productsController.index);
 
 /*** CREATE ONE PRODUCT ***/
 router.get("/create", productsController.create);
-router.post("/", productsController.store);
+router.post("/", upload.single("image"), productsController.store);
 
-// /*** GET ONE PRODUCT ***/
-// router.???('/:id/', productsController.detail);
+/*** GET ONE PRODUCT ***/
+router.get("/detail/:id", productsController.detail);
 
 // /*** EDIT ONE PRODUCT ***/
-// router.???('/:id/???', productsController.edit);
-// router.???('/:id', productsController.update);
+router.get("/edit/:id", productsController.edit);
+router.patch("/edit/:id", upload.single("image"), productsController.update);
 
 // /*** DELETE ONE PRODUCT***/
-// router.???('/:id', productsController.destroy);
+router.delete("/:id", productsController.destroy);
 
 module.exports = router;
